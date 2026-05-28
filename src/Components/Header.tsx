c15a7106ee61e8c5bb9132fabf7fca54ecc4e679
RVSSportsAcademy <147022527+RVSSportsAcademy@users.noreply.github.com>
2026-05-28 12:45:03 +0530

    Update Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
         <Link href="/">
           <h1 className="text-2xl font-bold">
-            RVS Sports Academy
+            RBS Sports Welfare Association
           </h1>
         </Link>
       </div>
